35bc0122f2db107adaf925b49e123aeef809a34b
Valde <randomvald0069@gmail.com>
2019-06-01 22:17:27 +0200

    Initial code

---
+++ src/src/main/java/HttpMain.java
@@ -0,0 +1,27 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class HttpMain {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/hello", new HelloHandler());
+        server.setExecutor(null);
+        server.start();
+    }
+
+    private static class HelloHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "Hello world!!!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream outputStream = exchange.getResponseBody();
+            outputStream.write(response.getBytes());
+            outputStream.close();
+        }
+    }
+}
